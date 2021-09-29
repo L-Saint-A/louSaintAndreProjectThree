efe5f6f98c2aad165039cc904e6e0058cf29da22
Lou <l_saint_andre@hotmail.com>
2021-09-29 19:11:40 -0400

    reactSetupAndAPICallSuccessful

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  const [weather, setWeather] = useState([]);
+
+
+  useEffect( ()=> {
+
+  
+  axios({
+    method: 'GET',
+    url: `http://api.worldweatheronline.com/premium/v1/weather.ashx?key=5006b4f52810441184b183358212809&q=Toronto&format=json&num_of_days=5`,
+    dataResponse: 'json',
+
+  }).then( (result)=> {
+    //save our response array to STATE
+    setWeather(result.data)
+  });
+}, []);  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Ooooooh, hello...</h1>
+
     </div>
   );
+
+
+
+
+
+  
 }
 
 export default App;
